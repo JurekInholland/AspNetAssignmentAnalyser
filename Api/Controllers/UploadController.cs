e70e29cb7f8e9cb4890145b308dd32faf5d0bef4
Jurek Baumann <jurekbaumann@me.com>
2023-02-26 23:35:33 +0100

    Debug production request headers

--- Api/Controllers/UploadController.cs
+++ Api/Controllers/UploadController.cs
@@ -8,12 +8,15 @@ namespace Api.Controllers;
 
 public class UploadController : BaseController
 {
+    private readonly ILogger<UploadController> _logger;
     private readonly IFileUploadService _fileUploadService;
     private readonly IEmailService _emailService;
     private readonly AppConfig _config;
 
-    public UploadController(IFileUploadService fileUploadService, IEmailService emailService, IOptions<AppConfig> config)
+    public UploadController(ILogger<UploadController> logger, IFileUploadService fileUploadService, IEmailService emailService,
+        IOptions<AppConfig> config)
     {
+        _logger = logger;
         _fileUploadService = fileUploadService;
         _emailService = emailService;
         _config = config.Value;
@@ -30,7 +33,17 @@ public class UploadController : BaseController
 
     private string? ParseRequestHeader(string headerKey)
     {
+        DebugLogReqHeaders();
         return Request.Headers.TryGetValue(headerKey, out var headerValue) ? headerValue.ToString() : null;
+    }
+
+    private void DebugLogReqHeaders()
+    {
+        _logger.LogInformation("Parsing request header");
+        foreach (var header in Request.Headers)
+        {
+            _logger.LogInformation("{HeaderKey}: {HeaderValue}", header.Key, header.Value);
+        }
     }
 
     [HttpGet("test", Name = nameof(TestSndMail))]
